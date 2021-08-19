cb9e95a6a49a5d2e1936d05d73961e5ae18f25ac
Prince Sana <88443169+princesana2000@users.noreply.github.com>
2021-08-19 19:35:04 +0530

    Add files via upload

---
+++ payment form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+   
+</head>
+<body>
+<div class="link"> <form><script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_Hj8ewKrTM1YY8r" async> </script> </form></div>
+        
+</body>
+</html>
